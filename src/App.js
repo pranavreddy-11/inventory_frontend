1cabf2b5a5ee1c83dba10dc9743d8612dde88d1f
Test User <test@example.com>
2026-01-18 15:32:30 +0530

    Update API URL to Render backend

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import TaglineSection from "./TaglineSection";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://inventory-backend-px9c.onrender.com",
 });
 
 function App() {
